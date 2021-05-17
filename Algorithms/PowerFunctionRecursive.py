3d02dadf175f5fabfda43e747235f3e1c4b8c717
AparnaVB <aparnavbalan@gmail.com>
2021-05-17 22:13:55 +0530

    Update PowerFunctionRecursive.py

--- Algorithms/PowerFunctionRecursive.py
+++ Algorithms/PowerFunctionRecursive.py
@@ -1,1 +1,18 @@
+def power(val, num):
+    if num == 0:
+        print("returning 1")
+        return 1
+    elif num % 2 == 0:
+        temp = power(val, num//2)
+        return temp * temp
+    elif num < 0:
+        return 1/power(val, -num)
+    elif num % 2 != 0:
+        return val * power(val, num-1)
+
+
+print(power(10, 4))
+print(power(3, 3))
+print(power(3, -3))
+print(power(2, 0))
 
