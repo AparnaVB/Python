61f8524fd3f048ef841ddbf7f30860ddef38d04c
AparnaVB <aparnavbalan@gmail.com>
2021-05-17 18:07:21 +0530

    Update PalindromeRecursion.py

--- Algorithms/PalindromeRecursion.py
+++ Algorithms/PalindromeRecursion.py
@@ -1,1 +1,15 @@
+def ispalindrome(val):
+    if len(val) in (0, 1):
+        return True
+    elif val[0] != val[-1]:
+        return False
+    else:
+        return ispalindrome(val[1:-1])
+
+
+val = 'rotor'
+print("The value {0} is Palindrome : {1}".format(val, ispalindrome(val)))
+
+val1 = 'retor'
+print("The value {0} is Palindrome : {1}".format(val, ispalindrome(val1)))
 
